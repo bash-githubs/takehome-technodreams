8abfb1079b7423bece9f77aa64900bc91c52e8fd
Joseph Bashorun <info.joolba@gmail.com>
2023-01-11 14:43:29 +0100

    Fix response message
    Signed-off-by: Joseph Bashorun <info.joolba@gmail.com>

--- leads_parser/views.py
+++ leads_parser/views.py
@@ -20,8 +20,6 @@ class LeadsManagementViewSet(viewsets.ViewSet):
                 status=status.HTTP_400_BAD_REQUEST,
             )
         data = request.FILES.get("file")
-        print("Data", data)
-        print("Request", request.data)
         data = base64.b64encode(data.read()).decode("utf-8")
         task = extract_csv_data.delay(data)
         return Response(
@@ -38,7 +36,7 @@ class LeadsManagementViewSet(viewsets.ViewSet):
         if task_result.status != "SUCCESS":
             return Response(
                 {
-                    "message": "task is still processing",
+                    "message": f"task is {task_result.status}",
                     "status": task_result.status,
                     "task_id": task_result.id,
                 },
